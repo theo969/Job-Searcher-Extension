ab71625f374c7b8f28a070aeeb6fa6c0087f180c
MuhammadJufri <sayyidmuhammad878@gmail.com>
2022-11-30 11:39:44 +0700

    Update the knexfile.ts file

--- packages/jse-backend/database/knexfile.ts
+++ packages/jse-backend/database/knexfile.ts
@@ -3,24 +3,24 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-import dotenv from 'dotenv';
-dotenv.config({ path: "../.env" });
-
-export default {
-  development: {
-    client: 'postgresql',
-    connection: {
-      port: Number(process.env.DB_PORT!),
-      database: process.env.DATABASE!,
-      user: process.env.DB_USER!,
-      password: process.env.DB_PASS!,
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-};
+ import dotenv from 'dotenv';
+ dotenv.config({ path: "../.env" });
+ 
+ export default {
+   development: {
+     client: 'postgresql',
+     connection: {
+       port: Number(process.env.DB_PORT!),
+       database: process.env.DATABASE!,
+       user: process.env.DB_USER!,
+       password: process.env.DB_PASS!,
+     },
+     pool: {
+       min: 2,
+       max: 10
+     },
+     migrations: {
+       tableName: 'knex_migrations'
+     }
+   },
+ };
